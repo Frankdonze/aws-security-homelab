9e0a3f6dd768d3a40aba178969f43f8b1905613a
frank donze <ftd0001@auburn.edu>
2025-09-07 05:01:09 -0400

    Initial terraform setup and first terraform script

---
+++ First-TF-Server.tf
@@ -0,0 +1,18 @@
+#----provider config----
+provider "aws" {
+	region = "us-east-1"
+}
+
+#----launch EC2 instance----
+resource "aws_instance" "my_ec2" {
+	ami = "ami-00ca32bbc84273381"
+	instance_type = "t2.nano"
+	
+
+	key_name = "sshkey"
+
+
+	tags = {
+	name = "First-TF-Server"
+}
+}
